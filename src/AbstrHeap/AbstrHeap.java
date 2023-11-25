aceeadc2585dd894f6c1f4c9ed1710e6f4d813b5
Oleksandr Mytro <st69631@upce.cz>
2023-11-25 20:20:00 +0100

    abstrheap

--- src/AbstrHeap/AbstrHeap.java
+++ src/AbstrHeap/AbstrHeap.java
@@ -1,4 +1,60 @@
 package AbstrHeap;
 
-public class AbstrHeap {
+public class AbstrHeap<K extends Comparable<K>> implements IAbstrHeap<K> {
+
+    int arr[];
+    int n;
+    @Override
+    public void vybuduj() {
+
+    }
+
+    @Override
+    public void prebuduj(K[] arr) {
+
+    }
+
+    @Override
+    public void zrus() {
+
+    }
+
+    @Override
+    public boolean jePrazdny() {
+        return false;
+    }
+
+    @Override
+    public void vloz(int data) {
+        n += 1;
+        arr[n-1] = data;
+        fixUp(arr, n-1);
+    }
+
+    private void fixUp(int[] arr, int i) {
+
+    }
+
+
+    @Override
+    public K odeberMax() {
+        return null;
+    }
+
+    @Override
+    public K zpristupniMax() {
+        return null;
+    }
+
+    @Override
+    public void vypis() {
+
+    }
+    private int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    private int parent(int i) {
+        return (i - 2) / 2;
+    }
 }
--- src/AbstrHeap/IAbstrHeap.java
+++ src/AbstrHeap/IAbstrHeap.java
@@ -1,4 +1,12 @@
 package AbstrHeap;
 
-public class IAbstrHeap {
+public interface IAbstrHeap<K extends Comparable<K>> {
+    void vybuduj();
+    void prebuduj(K[] arr);
+    void zrus();
+    boolean jePrazdny();
+    void vloz(int data);
+    K odeberMax();
+    K zpristupniMax();
+    void vypis();
 }
