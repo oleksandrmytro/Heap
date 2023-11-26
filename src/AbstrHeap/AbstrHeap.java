debb62c7fa2bd25c2360cf5320f54b697c841f02
Oleksandr Mytro <st69631@upce.cz>
2023-11-26 16:50:24 +0100

    abstrheap: vybuduj, prebuduj, zrus, vloz, odeberMax, zpristupniMax, firstLeafIndex

--- src/AbstrHeap/AbstrHeap.java
+++ src/AbstrHeap/AbstrHeap.java
@@ -69,7 +69,6 @@ public class AbstrHeap<K extends Comparable<K>> implements IAbstrHeap<K> {
     public void zrus() {
         this.n = 0;
         this.arr = null;
-        arr = null;
     }
 
     @Override
